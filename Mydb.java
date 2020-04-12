2b79d247e74cf21df63bf06b39f6ab456db67170
VashishthSingh <singhshikharmuni@gmail.com>
2020-04-12 10:31:16 +0530

    ChartJsRDCUGraph

---
+++ Mydb.java
@@ -0,0 +1,19 @@
+package com.angulatpac;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Mydb {
+	  Connection con;
+	  
+	  public Connection getCon(){
+	    
+	    try {
+	      Class.forName("com.mysql.jdbc.Driver");
+	      con = DriverManager.getConnection("jdbc:mysql://localhost:3306/report", "root", "password");
+	    } catch (Exception e) {
+	      System.out.println(e);
+	    }
+	    return con;
+	  }
+}
